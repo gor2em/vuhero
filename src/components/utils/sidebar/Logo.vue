4d0c3b36a7cba58668648368ab0a4b18cf62c5aa
gorkem <grkem.squash99@gmail.com>
2024-02-11 17:51:40 +0300

    update

--- src/components/utils/sidebar/Logo.vue
+++ src/components/utils/sidebar/Logo.vue
@@ -6,9 +6,8 @@ defineProps({
 <template>
   <div class="px-2 text-2xl font-bold">
     <a>
-      <span class="text-teal-400">.</span>
       <span class="text-rose-600">.</span>
-      <span>{{ isSidebarOpen ? "monibil" : "m" }}</span>
+      <span>{{ isSidebarOpen ? "vuhero" : "v" }}</span>
     </a>
   </div>
 </template>
--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,9 +1,14 @@
 <script setup></script>
 <template>
-  <div v-for="i in 100" :key="i" class="text-justify">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, ut autem
-    debitis, eos sunt assumenda qui veritatis amet enim atque, neque modi
-    recusandae illo voluptatum. Dignissimos architecto totam tempore. Rem!
+  <div>
+    
+    <h1 class="mt-4 mb-8 text-6xl font-bold text-red-600">hello, huawei cloud</h1>
+
+    <div v-for="i in 100" :key="i" class="text-justify">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, ut autem
+      debitis, eos sunt assumenda qui veritatis amet enim atque, neque modi
+      recusandae illo voluptatum. Dignissimos architecto totam tempore. Rem!
+    </div>
   </div>
 </template>
 <style scoped></style>
