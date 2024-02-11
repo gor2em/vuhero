2342c7cd9fc00ea479cd154f316493fdde2f25b3
gorkem <grkem.squash99@gmail.com>
2024-02-11 21:40:44 +0300

    test

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -4,6 +4,7 @@
     
     <h1 class="mt-4 mb-8 text-6xl font-bold text-red-600">hello, huawei cloud</h1>
     test
+    test2
     <div v-for="i in 100" :key="i" class="text-justify">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, ut autem
       debitis, eos sunt assumenda qui veritatis amet enim atque, neque modi
